4733615f162a6c600e127797d37898e33d5f3f36
Brianto2 <91679392+Brianto2@users.noreply.github.com>
2022-01-27 09:53:08 -0500

    Add files via upload
    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from tkinter.filedialog import askdirectory
+
+import Dupsearch
+import tkinter as tk
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    Dupsearch.find_dupes(128)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
